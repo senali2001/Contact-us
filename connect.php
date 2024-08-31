4a3d195dd7fb449c867a78cc7ae522b445fb76bd
senali2001 <navodhyasenali@gmail.com>
2024-08-31 11:45:07 +0530

    sent email admin and user about massage throught phpmailer

--- connect.php
+++ connect.php
@@ -1,4 +1,9 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/vendor/autoload.php'; // Include PHPMailer's autoload file
+
 
 $full_name = $_POST['full_name'];
 $email = $_POST['email'];
@@ -17,7 +22,49 @@ if ($conn->connect_error) {
     $stmt->execute();
     $stmt->close();
     $conn->close();
+}   
+// Sending the email
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'navodhyasenali@gmail.com';             // SMTP username
+    $mail->Password   = 'bekbpldlbnlrlhbv';                     // SMTP password
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 587;
+
+
     
+    //Recipients
+    $mail->setFrom('navodhyasenali@gmail.com', 'DREAM SCAPE');
+    $mail->addAddress($email, $full_name);  // Add a recipient
+    $mail->addReplyTo('navodhyasenali@gmail.com', 'More Information');
+
+    // Content for User
+    $mail->isHTML(true);                                        // Set email format to HTML
+    $mail->Subject = 'Contact Us Form Submission Dream Scape Tour';
+    $mail->Body    = "Dear $full_name,<br><br>Thank you for contacting us. You will be connected with our agent as soon as possible. Here is a copy of your message:<br><br>$massage";
+    $mail->send();
+
+
+    $mail->clearAddresses();
+    $mail->clearAttachments();
+
+
+    $mail->addAddress('navodhyasenali@gmail.com');
+    $mail->isHTML(true);                                        // Set email format to HTML
+    $mail->Subject = 'Contact Us Form Submission Dream Scape Tour';
+    $mail->Body    = " $full_name want to contact our Support Team ,<br><br> Here is a copy of message:<br><br>$massage";
+    
+    $mail->send();
+
+
+    
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
 
 //sweet text for contact page after massage sent
@@ -45,7 +92,3 @@ if ($conn->connect_error) {
         </script>
     </body>
     </html>
-<?php
-}
-
-?>
